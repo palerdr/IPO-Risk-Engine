80d702aaaf33fcc98ec00e9f74e4979c94b1385b
James Cenawood <jdewitte632@gmail.com>
2026-02-15 17:57:35 -0500

    chore: untrack local assistant and archive files

--- archive/v0_state_inference/state_data/build_supervised.py
+++
@@ -1,7 +0,0 @@
-import polars as pl
-"""
-Joins features and labels into a supervised dataset
-"""
-def build_supervised(features_lf: pl.LazyFrame, labels_lf:pl.LazyFrame) -> pl.LazyFrame:
-    #inner join on timestamps
-    return features_lf.join(labels_lf, on="ts", how="inner")
